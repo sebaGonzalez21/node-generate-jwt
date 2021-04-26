dcb258ed8cf7602daf5b044a23a85f3a45312860
Sebastian Gonzalez <ext_sagonzalezp@falabella.cl>
2021-04-26 16:04:01 -0400

    modifications

--- jwt-rs256/routes/routes.js
+++ jwt-rs256/routes/routes.js
@@ -8,21 +8,25 @@ logger.level = 'all';
 router.post('/api/v1/jwt', (req, res) => {
     try {
 
-        let emailCuentaServicio = String(req.body.email);
+        //let emailCuentaServicio = String(req.body.email);
         let segundosVencimiento = Number(req.body.seconds);
         let privateKey = String(req.body.privateKey);
-        let urlService = String(req.body.urlService);
+        let client = String(req.body.client);
         let algorithm = String(req.body.algorithm);
+
+        var header = {
+          kid: client,
+        }
 
         const payload = {
             iat: new Date().getTime(),
             exp: new Date().getTime() + segundosVencimiento,
-            iss: emailCuentaServicio,
-            aud: urlService,
-            sub: emailCuentaServicio,
-            email: emailCuentaServicio
+            //iss: emailCuentaServicio,
+            //kid: client,
+            //sub: emailCuentaServicio,
+            //email: emailCuentaServicio
         };
-        const token = 'Bearer ' + jwt.sign(payload, privateKey, { algorithm });
+        const token = 'Bearer ' + jwt.sign(payload, privateKey, { header, algorithm });
         logger.info("token generado exitosamente");
         res.status(200).json({ token: token });
     } catch (err) {
